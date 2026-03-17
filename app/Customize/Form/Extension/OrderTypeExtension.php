c8ac55880f5ac908479e4646a9ef3d26c810e1d3
Khoa Dinh <khoa.dinhtps@gmail.com>
2026-03-17 16:44:14 +0700

    up

--- app/Customize/Form/Extension/OrderTypeExtension.php
+++ app/Customize/Form/Extension/OrderTypeExtension.php
@@ -6,15 +6,21 @@ use Eccube\Form\Type\Shopping\OrderType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 
 class OrderTypeExtension extends AbstractTypeExtension
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('user_note', TextareaType::class, [
-            'required' => false,
-            'label' => 'front.shopping.user_note',
-        ]);
+           if (!$options['skip_add_form']) {
+            $builder->add('user_note', TextareaType::class, [
+                'required' => false,
+                'label' => 'front.shopping.user_note',
+                'constraints' => [
+                    new Length(['max' => 4000]),
+                ],
+            ]);
+        }
     }
 
     public static function getExtendedTypes(): iterable
